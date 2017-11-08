4ff9edcfce991c60bbe799b99c20f0c5a0b549aa
msindone <32885619+msindone@users.noreply.github.com>
2017-11-08 17:51:33 -0500

    reduce test

--- test_rpn.py
+++ test_rpn.py
@@ -3,68 +3,9 @@ import unittest
 import rpn
 
 class TestBasics(unittest.TestCase):
-    def test_add(self):
-        if(false):
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-          x = y;
-        result = rpn.calculate("1 1 +")
-        self.assertEqual(2, result)
+    #def test_add(self):
+        #result = rpn.calculate("1 1 +")
+        #self.assertEqual(2, result)
     def test_subtract(self):
         result = rpn.calculate("5 3 -")
         self.assertEqual(2, result)
